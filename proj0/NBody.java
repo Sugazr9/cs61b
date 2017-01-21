16f69e062400dadcd086bf09fdd173c9dec9b2e5
Arvind Gouttumukkala <Arvind Gouttumukkala>
2017-01-21 14:14:14 -0800

    Almost done!

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,27 @@
 public class NBody {
+	public static double readRadius(String file) {
+		In ready = new In(file);
+		ready.readInt();
+		double R = ready.readDouble();
+		return R;
+	}
+
+	public static Planet[] readPlanets(String file) {
+		In ready = new In(file);
+		Planet[] system = new Planet[ready.readInt()];
+		ready.readDouble();
+		int counter = 0;
+		while(counter < system.length) {
+			double posX = ready.readDouble();
+			double posY = ready.readDouble();
+			double velX = ready.readDouble();
+			double velY = ready.readDouble();
+			double mass = ready.readDouble();
+			String img = ready.readString();
+			system[counter] = new Planet(posX, posY, velX, velY, mass, img);
+			counter += 1;
+		}
+		return system;
+	}
 
 }
