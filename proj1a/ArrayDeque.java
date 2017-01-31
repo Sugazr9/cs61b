0f9ebaed2d566127a1dbe15b3cab5b32f5b621b4
Arvind Gouttumukkala <Arvind Gouttumukkala>
2017-01-31 05:57:21 -0800

    Think all is fixed

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -6,70 +6,77 @@ public class ArrayDeque<Item> {
     private int size;
     private int front_index;
     private int back_index;
-    private int extension;
     public ArrayDeque() {
         storage = (Item[]) new Object[8];
         size = 0;
         front_index = 0;
         back_index = 0;
-        extension = 0;
 
     }
     private int next(int index) {
-        if (isEmpty()) {
-            return 0;
+        if (index == back_index) {
+            return front_index;
         }
+        return helper_next(index);
+    }
+    private int helper_next(int index) {
         if (index == storage.length - 1) {
             return 0;
         }
         return index + 1;
     }
     private int previous(int index) {
-        if (isEmpty()) {
-            return 0;
+        if (index == front_index) {
+            return back_index;
         }
+        return helper_previous(index);
+    }
+    private int helper_previous(int index) {
         if (index == 0) {
             return storage.length - 1;
         }
         return index - 1;
     }
     private void resize(String command) {
+        int pointer = front_index;
         if (command.equals("increase")) {
             Item[] a = (Item[]) new Object[storage.length * 2];
-            System.arraycopy(storage, front_index, a, 0, extension);
-            System.arraycopy(storage, 0, a, extension, size - extension);
+            System.arraycopy(storage, pointer, a, 0, 1);
+            pointer = next(pointer);
+            while (pointer != front_index) {
+                System.arraycopy(storage, pointer, a, 0, 1);
+                pointer = next(pointer);
+            }
             storage = a;
         }
         else {
             Item[] a = (Item[]) new Object[storage.length / 2];
-            System.arraycopy(storage, front_index, a, 0, extension);
-            System.arraycopy(storage, 0, a, extension, size - extension);
+            System.arraycopy(storage, pointer, a, 0, 1);
+            pointer = next(pointer);
+            while (pointer != front_index) {
+                System.arraycopy(storage, pointer, a, 0, 1);
+                pointer = next(pointer);
+            }
             storage = a;
         }
-
         front_index = 0;
         back_index = previous(size);
-        extension = 0;
     }
     public void addFirst(Item x) {
         if (size == storage.length) {
             resize("increase");
         }
-        front_index = previous(front_index);
+        front_index = helper_previous(front_index);
         storage[front_index] = x;
         size++;
-        if (front_index >= back_index) {
-            extension++;
-        }
     }
     public void addLast(Item x) {
         if (size == storage.length) {
             resize("increase");
         }
-        back_index = next(back_index);
+        back_index = helper_next(back_index);
         storage[back_index] = x;
         size++;
-        }
     }
     public boolean isEmpty() {
         if (size == 0) {
@@ -82,9 +89,13 @@ public class ArrayDeque<Item> {
     }
     public void printDeque() {
         int pointer = front_index;
-        while (storage[pointer] != null) {
-            System.out.print(storage[pointer] + " ");
-            pointer = next(pointer);
+        if (isEmpty()) {
+        } else {
+            while (pointer != back_index) {
+                System.out.print(storage[pointer] + " ");
+                pointer = next(pointer);
+            }
+            System.out.println(storage[pointer]);
         }
     }
     public Item removeFirst() {
@@ -95,10 +106,6 @@ public class ArrayDeque<Item> {
         size--;
         storage[front_index] = null;
         front_index = next(front_index);
-        extension--;
-        if (front_index <= back_index) {
-            extension = 0;
-        }
         if (size < 16) {
             if (storage.length * 0.1 > size && storage.length > 5) {
                 resize("decrease");
@@ -118,9 +125,6 @@ public class ArrayDeque<Item> {
         Item val = storage[back_index];
         size--;
         storage[back_index] = null;
-        if (back_index == 0) {
-            extension = 0;
-        }
         back_index = previous(back_index);
         if (size < 16) {
             if (storage.length * 0.1 > size && storage.length > 5) {
@@ -135,11 +139,12 @@ public class ArrayDeque<Item> {
         return val;
     }
     public Item get(int index) {
+        int num_to_break = storage.length - front_index;
         if (index >= size) {
             return null;
         }
-        if (index >= extension) {
-            return storage[index - extension];
+        if (index >= num_to_break) {
+            return storage[index - num_to_break];
         }
         else {
             return storage[front_index + index];
