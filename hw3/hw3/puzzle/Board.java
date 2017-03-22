e7cef33acd7273ae73e28d82047f4f8ddd354d52
Arvind Gouttumukkala <Arvind Gouttumukkala>
2017-03-22 13:20:20 -0700

    added goal estimation

--- hw3/hw3/puzzle/Board.java
+++ hw3/hw3/puzzle/Board.java
@@ -1,13 +1,20 @@
 package hw3.puzzle;
 
 import java.util.Arrays;
+import edu.princeton.cs.algs4.Queue;
 
 public class Board implements WorldState{
     private int[][] current;
     private final int N;
+
     public Board(int[][] tiles) {
-        current = tiles.clone();
         N = tiles.length;
+        current = new int[N][N];
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) {
+                current[row][col] = tiles[row][col];
+            }
+        }
     }
 
     public int tileAt(int i, int j) {
@@ -23,15 +30,67 @@ public class Board implements WorldState{
 
     @Override
     public Iterable<WorldState> neighbors() {
-        return null;
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == 0) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = 0;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = 0;
+                }
+            }
+        }
+        return neighbors;
     }
 
     public int hamming() {
-        return 0;
+        int misplaced = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!(i == N - 1 && j == N - 1)) {
+                    if (current[i][j] != N * i + j + 1) {
+                        misplaced++;
+                    }
+                }
+            }
+        }
+        return misplaced;
     }
 
     public int manhattan() {
-        return 0;
+        int total = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int val = current[i][j];
+                if (val != 0 && val != N * i + j + 1) {
+                        int row = (val - 1) / N;
+                        int col = (val - 1) % N;
+                        int distance = Math.abs(i - row) + Math.abs(j - col);
+                        total += distance;
+                }
+            }
+        }
+        return total;
     }
 
     @Override
@@ -43,11 +102,10 @@ public class Board implements WorldState{
     public boolean isGoal() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if(i == N -1 && j == N - 1) {
-                    break;
-                }
-                if(current[i][j] != N * i + j + 1) {
-                    return false;
+                if (!(i == N - 1 && j == N - 1)) {
+                    if (current[i][j] != N * i + j + 1) {
+                        return false;
+                    }
                 }
             }
         }
@@ -80,5 +138,4 @@ public class Board implements WorldState{
         s.append("\n");
         return s.toString();
     }
-
 }
