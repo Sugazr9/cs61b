d0c7aee8b978c2821016935e841ee3baa3af4f2a
Arvind Gouttumukkala <Arvind Gouttumukkala>
2017-04-15 23:41:19 -0700

    finished rastering

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -16,6 +16,8 @@ public class Rasterer {
     private final double bottomMostLat;
     private final double[] LonDPPs;
     private final double[] LonPerPic;
+    private final double[] LatPerPic;
+    String directory;
     /** imgRoot is the name of the directory containing the images.
      *  You may not actually need this for your class. */
     public Rasterer(String imgRoot) {
@@ -25,9 +27,12 @@ public class Rasterer {
         bottomMostLat = 37.82280243352756;
         LonDPPs = new double[8];
         LonPerPic = new double[8];
+        LatPerPic = new double[8];
+        directory = imgRoot;
         for (int i = 1; i < 8; i++) {
             LonDPPs[i] = (rightMostLon - leftMostLon) / (Math.pow(2, i) * 256);
             LonPerPic[i] = LonDPPs[i] * 256;
+            LatPerPic[i] = (topMostLat - bottomMostLat) / Math.pow(2, i);
         }
     }
 
@@ -66,24 +71,99 @@ public class Rasterer {
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
         Map<String, Object> results = new HashMap<>();
-        double LonDDP = (params.get("lrlon") - params.get("ullon")) / params.get("w");
-        int level = 1;
-        for (int i = 1; i < 8; i++) {
-            if(LonDPPs[i] < LonDDP) {
-                level = i;
-                break;
-            } else if (i == 7) {
-                level = 7;
-            }
+        operate(params, results);
+        if (!((boolean) results.get("query_success"))) {
+            return results;
         }
-        results.put("depth", level);
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        String[][] raster = new String[(int) results.get("height")][(int) results.get("width")];
+        String start = (String) results.get("start");
+        for (int i = 0; i < raster.length; i++) {
+            String edge = start;
+            for (int j = 0; j < raster[0].length; j++) {
+                if (edge == "") {
+                    raster[i][j] = edge;
+                } else {
+                    raster[i][j] = directory + edge + ".png";
+                }
+                edge = getRight(edge);
+            }
+            start = getBottom(start);
+        }
+        results.remove("start");
+        results.remove("height");
+        results.remove("length");
+        results.put("render_grid", raster);
         return results;
     }
 
+    private void operate(Map<String, Double> params, Map<String, Object> results) {
+        double ullat = params.get("ullat");
+        double lrlat = params.get("lrlat");
+        double ullon = params.get("ullon");
+        double lrlon = params.get("lrlon");
+        if (ullon > rightMostLon || lrlon < leftMostLon) {
+            results.put("query_success", false);
+            return;
+        } else if (ullat < bottomMostLat || lrlat > topMostLat){
+            results.put("query_success", false);
+            return;
+        }
+        double LonDDP = (lrlon - ullon) / params.get("w");
+        String start = "";
+        double left = leftMostLon;
+        double top = topMostLat;
+        for (int i = 1; i < 8; i++) {
+            double LonCurr = LonPerPic[i];
+            double LatCurr = LatPerPic[i];
+            if (left - ullon > LonCurr) {
+                left += LonCurr;
+                if (top - ullat > LatCurr) {
+                    top -= LatCurr;
+                    start += "4";
+                    if (LonCurr < LonDDP) {
+                        break;
+                    }
+                } else {
+                    start += "2";
+                    if (LonCurr < LonDDP) {
+                        break;
+                    }
+                }
+            } else {
+                if (top - ullat > LatCurr) {
+                    top -= LatCurr;
+                    start += "3";
+                    if (LonCurr < LonDDP) {
+                        break;
+                    }
+                } else {
+                    start += "1";
+                    if (LonCurr < LonDDP) {
+                        break;
+                    }
+                }
+            }
+        }
+        int level = start.length();
+        results.put("depth", level);
+        double LonPP = LonPerPic[level];
+        double LatPP = LatPerPic[level];
+        results.put("start", start);
+        results.put("raster_ul_lon", left);
+        results.put("raster_ul_lat", top);
+        Double l = (lrlon - left) / LonPP + 1;
+        results.put("length", l.intValue());
+        results.put("raster_lr_lon", left + l.intValue() * LonPP);
+        Double h = (lrlat - top) / LatPP + 1;
+        results.put("height", h.intValue());
+        results.put("raster_lr_lat", top - h.intValue() * LatPP);
+        results.put("query_success", true);
+    }
+
     private String getRight(String image) {
-        if (image.matches("*[24]")) {
+        if (image.matches("*(2|4)")) {
+            return "";
+        } else if(image.equals("")) {
             return "";
         }
         int curr = Integer.getInteger(image);
@@ -102,7 +182,9 @@ public class Rasterer {
     }
 
     private String getBottom(String image) {
-        if (image.matches("*[34]")) {
+        if (image.matches("*(3|4)")) {
+            return "";
+        } else if(image.equals("")) {
             return "";
         }
         int curr = Integer.getInteger(image);
